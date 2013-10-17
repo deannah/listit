82d2c0e9c33c09c4df18c32d294f47861f803803
deannah <deannah@mit.edu>
2013-10-17 08:42:01 -0400

    [Webapp] Theoretically syncs when note is saved from omnibox.

--- src/js/platforms/web/Models/Omnibox.js
+++ src/js/platforms/web/Models/Omnibox.js
@@ -1,16 +1,9 @@
 (function(L) {
-  // L.views.OmniboxView = L.views.OmniboxView.extend({
-//     
-    // L.views.OmniboxView.prototype.save();
-  // });
-  // L.models.Server.initialized = function() {
-    // L.models.Server.prototype.initialized();
-  // };
-  L.models.Omnibox.initialized = function() {
-    L.models.Omnibox.prototype.initialized();
-    this.listenTo(this, "note-created", function() {
-      L.Server.syncNotes();
-    });
-  };
+  ListIt.lvent.once('setup:models:after', function(L) {
+    L.omnibox.on( "note-created", function() {
+      console.log("wompity woo");
+      L.server.syncNotes();
+    })
+  });
   
 })(ListIt);
