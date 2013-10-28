8ba423567966c101c6c6c3943e82f02774af38e3
deannah <deannah@mit.edu>
2013-10-28 16:15:20 -0400

    [Web] Syncing on event add note event

---
+++ src/js/platforms/web/models/notes.js
@@ -0,0 +1,8 @@
+(function(L) {
+  ListIt.lvent.once('setup:models:after', function(L) {
+    L.notebook.get("notes").on("add", function() {
+      console.log("Syncing on note add.");
+      L.server.pushNotes();
+    })
+  });
+})(ListIt);
